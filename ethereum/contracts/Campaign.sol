7903ff344d5d25a4eabe44be7da153e9f233c1db
dennis <dennis@debian-BULLSEYE-live-builder-AMD64>
2021-08-30 18:49:52 +1000

    D

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,63 +1,83 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity ^0.8.7;
+// Deploys instances of the campaign contract
+// Cost of deployment is on the user
+// Factory is deployed to the blockchain
 contract CampaignFactory {
-    address[] public deployedCampaigns;
+    // Addresses of all the deployed campaigns
+    Campaign[] public deployedCampaigns;
 
+// Deploys a new instance of a Campaign and stores the resulting addresses
     function createCampaign(uint minimum) public {
-        address newCampaign = new Campaign(minimum, msg.sender);
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
-
-    function getDeployedCampaigns() public view returns (address[]) {
+// Returns a list of all deployed campaigns
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
         return deployedCampaigns;
     }
 }
 
 contract Campaign {
+    // similar to a class
     struct Request {
+        // Describes why the request is being created
         string description;
+        // Amount of money the manager wants to send to the vendor
         uint value;
-        address recipient;
+        // Address that the money will be sent to
+        address payable recipient;
+        // True is the request has already been processed (money sent)
         bool complete;
         uint approvalCount;
         mapping(address => bool) approvals;
     }
-
-    Request[] public requests;
+    // List of requests that the manager has created - Mapping of Request Strut
+     mapping (uint => Request) requests;
+    // Address of the person who is managing the campaign
     address public manager;
+    // Minimum donation reqired to be a contributor or 'approver'
     uint public minimumContribution;
+    // List of addresses for every person who donated money
     mapping(address => bool) public approvers;
     uint public approversCount;
+    
+    uint requestIndex = 0;
+    
+    uint public requestsCount;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-
-    function Campaign(uint minimum, address creator) public {
-        manager = creator;
-        minimumContribution = minimum;
-    }
-
+    // Constructor function that sets the minimum contribtion and the address
+   constructor(uint minimum, address creator) {
+		manager = creator;
+		minimumContribution = minimum;
+	}
+    // Called when someone wants to donate money to the campaign and become an 'approver'
     function contribute() public payable {
+        // msg.value = amount in wei that has someone has sent
         require(msg.value > minimumContribution);
 
         approvers[msg.sender] = true;
         approversCount++;
     }
+// Called by the manager to create a new 'spending request'
+    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+        Request storage request = requests[requestIndex];
 
-    function createRequest(string description, uint value, address recipient) public restricted {
-        Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
-        });
+		request.description = description;
+		request.recipient = recipient;
+		request.value = value;
+		request.complete = false;
+		request.approvalCount = 0;
 
-        requests.push(newRequest);
+		requestIndex++;
+		requestsCount++;
     }
-
+// Called by each contributor to to approve a spending request
     function approveRequest(uint index) public {
         Request storage request = requests[index];
 
@@ -67,7 +87,7 @@ contract Campaign {
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
-
+// After a request had gotten enough approvals, the manager can call this to get money sent to the vendor
     function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
 
@@ -83,14 +103,14 @@ contract Campaign {
       ) {
         return (
           minimumContribution,
-          this.balance,
-          requests.length,
+          address(this).balance,
+          requestsCount,
           approversCount,
           manager
         );
     }
 
     function getRequestsCount() public view returns (uint) {
-        return requests.length;
+        return requestsCount;
     }
 }
